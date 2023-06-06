400743b88a7a3c448382aa4a0e46eb1d8894da37
Omar <omar0642624692@gmail.com>
2023-06-06 16:36:08 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for a in range(0, 9):
     for b in range(1, 10):
         if b > a:
             if b < 9 or a < 8:
-                print(f"{:d}{:d}".format(a, b), end=", ")
+                print("{:d}{:d}".format(a, b), end=", ")
             else:
-                print(f"{:d}{:d}".format(a, b))
+                print("{:d}{:d}".format(a, b))
