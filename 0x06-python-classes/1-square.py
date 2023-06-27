acb12cfd213a3b4900c128becda86893f1052a8f
Omar <omar0642624692@gmail.com>
2023-06-27 16:37:01 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""empty class Square"""
+"""
+empty class Square
+"""
 
 
 class Square:
-    """an empty class Square that defines a square"""
-
+    """
+	an empty class Square that defines a square
+	"""
     pass
