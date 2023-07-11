d1efac5ecf12604c9a3c90de2edb559d19b65dbc
Omar <omar0642624692@gmail.com>
2023-07-10 18:52:19 -0700

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Inherits from class Rectangle and BaseGeometry"""
+    def __init__(self, size):
+        """Initializes the values"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """Prints the description of the Rectangle"""
+        string = '[Square] ' + str(self.__size) + '/' + str(self.__size)
+        return string
