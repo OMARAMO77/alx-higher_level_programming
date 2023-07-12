b90670c9ffc367f6c824038812a54044be361bcf
Omar <omar0642624692@gmail.com>
2023-07-11 17:35:31 -0700

    a class Student that defines a student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""definition of student"""
+
+
+class Student:
+    """a class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes instance data"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a
+        Student instance"""
+        dictionary = self.__dict__
+        if attrs is None:
+            return dictionary
+
+        d = {k: v for k, v in dictionary.items() if k in attrs}
+        return d
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of the Student instance
+        """
+        self.__dict__.update(json)
