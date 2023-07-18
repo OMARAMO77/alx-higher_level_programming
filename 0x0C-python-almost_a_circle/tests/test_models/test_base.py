b6ab3ba6cc19ca794143650f98d5dc6b865ce3ee
Omar <omar0642624692@gmail.com>
2023-07-17 19:03:54 -0700

    test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,20 +1,46 @@
 import unittest
-from models.base import Base
+import json
+from base import Base
 
 
-class BaseTestCase(unittest.TestCase):
-    def test_id_auto_increment(self):
-        b1 = Base()
-        b2 = Base()
-        b3 = Base()
+class TestBase(unittest.TestCase):
 
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 3)
+    def test_to_json_string(self):
+        """Test when list_dictionaries is None"""
+        result = Base.to_json_string(None)
+        self.assertEqual(result, "[]")
 
-    def test_id_provided(self):
-        b4 = Base(12)
-        b5 = Base()
+        """Test when list_dictionaries is an empty list"""
+        result = Base.to_json_string([])
+        self.assertEqual(result, "[]")
 
-        self.assertEqual(b4.id, 12)
-        self.assertEqual(b5.id, 4)
+        """Test when list_dictionaries contains data"""
+        list_dictionaries = [{'id': 1, 'name': 'John'}, {'id': 2, 'name': 'Jane'}]
+        expected_json = json.dumps(list_dictionaries)
+        result = Base.to_json_string(list_dictionaries)
+        self.assertEqual(result, expected_json)
+
+    def test_save_to_file(self):
+        """Test when list_objs is None"""
+        Base.save_to_file(None)
+        with open("Base.json", "r") as file:
+            result = file.read()
+        self.assertEqual(result, "[]")
+
+        """Test when list_objs is an empty list"""
+        Base.save_to_file([])
+        with open("Base.json", "r") as file:
+            result = file.read()
+        self.assertEqual(result, "[]")
+
+        """Test when list_objs contains objects"""
+        obj1 = Base(1)
+        obj2 = Base(2)
+        Base.save_to_file([obj1, obj2])
+        with open("Base.json", "r") as file:
+            result = file.read()
+        expected_json = '[{"id": 1}, {"id": 2}]'
+        self.assertEqual(result, expected_json)
+
+if __name__ == '__main__':
+    unittest.main()
