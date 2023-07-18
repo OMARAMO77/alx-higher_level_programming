e573cf2b4256bf2a6cbc1787f13c94b94251e47b
Omar <omar0642624692@gmail.com>
2023-07-17 19:04:31 -0700

    test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,38 +1,24 @@
 import unittest
 from models.rectangle import Rectangle
 
+class TestRectangle(unittest.TestCase):
 
-class RectangleTestCase(unittest.TestCase):
-    def test_attributes(self):
-        r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r1.width, 10)
-        self.assertEqual(r1.height, 2)
-        self.assertEqual(r1.x, 0)
-        self.assertEqual(r1.y, 0)
+    def test_init(self):
+        """Test initialization with default values"""
+        rectangle = Rectangle(5, 10)
+        self.assertEqual(rectangle.width, 5)
+        self.assertEqual(rectangle.height, 10)
+        self.assertEqual(rectangle.x, 0)
+        self.assertEqual(rectangle.y, 0)
+        self.assertIsNotNone(rectangle.id)
 
-        r2 = Rectangle(2, 10)
-        self.assertEqual(r2.id, 2)
-        self.assertEqual(r2.width, 2)
-        self.assertEqual(r2.height, 10)
-        self.assertEqual(r2.x, 0)
-        self.assertEqual(r2.y, 0)
+        """Test initialization with custom values"""
+        rectangle = Rectangle(10, 20, 2, 3, 1)
+        self.assertEqual(rectangle.width, 10)
+        self.assertEqual(rectangle.height, 20)
+        self.assertEqual(rectangle.x, 2)
+        self.assertEqual(rectangle.y, 3)
+        self.assertEqual(rectangle.id, 1)
 
-        r3 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(r3.id, 12)
-        self.assertEqual(r3.width, 10)
-        self.assertEqual(r3.height, 2)
-        self.assertEqual(r3.x, 0)
-        self.assertEqual(r3.y, 0)
-
-    def test_setters(self):
-        r4 = Rectangle(5, 5)
-        r4.width = 15
-        r4.height = 20
-        r4.x = 3
-        r4.y = 4
-
-        self.assertEqual(r4.width, 15)
-        self.assertEqual(r4.height, 20)
-        self.assertEqual(r4.x, 3)
-        self.assertEqual(r4.y, 4)
+if __name__ == '__main__':
+    unittest.main()
