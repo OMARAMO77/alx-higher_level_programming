f5001e62c499dfec70c2c1bf94d1301428d57e7c
Omar <omar0642624692@gmail.com>
2023-08-16 15:30:05 -0700

    4-never_empty.sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
--- a script that creates the table force_name
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
+-- a script that creates the table id_not_null
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
 );
