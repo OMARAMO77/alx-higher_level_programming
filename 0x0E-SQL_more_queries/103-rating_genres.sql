ca19cf6291c4f805b6055ab3ebfdc38ef9ee7847
Omar <omar0642624692@gmail.com>
2023-08-18 11:08:50 -0700

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,10 +1,9 @@
 -- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating
-SELECT tv_genres.name,
-COALESCE(SUM(tv_show_ratings.rate), 0) AS rating_sum
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating_sum
 FROM tv_genres
 LEFT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN hbtn_0d_tvshows_rate.tv_show_ratings
+LEFT JOIN tv_show_ratings
 ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.id
 ORDER BY rating_sum DESC;
