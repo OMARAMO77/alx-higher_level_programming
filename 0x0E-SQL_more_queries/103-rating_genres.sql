553720920c81253d848849b79c5277f66e58cd7c
Omar <omar0642624692@gmail.com>
2023-08-18 11:13:17 -0700

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -6,4 +6,4 @@ ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_show_ratings
 ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.id
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
