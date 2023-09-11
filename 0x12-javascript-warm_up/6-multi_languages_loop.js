f5a40c6105a31e822cf927ad1465c83de0e81e3c
Omar <omar0642624692@gmail.com>
2023-09-11 15:56:52 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
 const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let a = 0; a < strings.length; ++a) {
-  console.log(strings[a]);
+for (const string of strings) {
+  console.log(string);
 }
