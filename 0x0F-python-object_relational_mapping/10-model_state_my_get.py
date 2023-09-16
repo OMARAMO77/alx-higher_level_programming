2abff1695300fcb31da3fa80c237ce5178e8a571
Omar <omar0642624692@gmail.com>
2023-09-16 13:21:36 -0700

    a script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,9 +10,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
 
     session = Session()
