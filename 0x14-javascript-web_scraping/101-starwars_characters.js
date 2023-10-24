1038f72b3f0f08e45d2275ef3b3decd731210faa
Omar <omar0642624692@gmail.com>
2023-10-24 15:59:33 -0700

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
 const movieID = process.argv[2];
-const apiurl = 'https://swapi-api.alx-tools.com//api/films/' + movieID;
+const apiurl = 'https://swapi-api.alx-tools.com/api/films/' + movieID;
 request(apiurl, function (err, response, body) {
-  if (err) {
-    console.error(err);
-  }
-  const jsonData = JSON.parse(body).characters;
-  for (const character of jsonData) {
-    request(character, function (error, response, content) {
-      if (error) {
-        console.log(error);
-      }
-      const data = JSON.parse(content);
-      console.log(data.name);
-    });
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, content) {
+    if (!error) {
+      console.log(JSON.parse(content).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
