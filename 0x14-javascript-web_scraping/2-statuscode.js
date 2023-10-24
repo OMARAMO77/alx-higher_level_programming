eaf3990e5980a7ff4ad069e99646ae73ba7b512a
Omar <omar0642624692@gmail.com>
2023-10-24 12:59:44 -0700

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,6 +6,6 @@ request(url, function (err, response) {
   if (err) {
     console.error(err);
   } else {
-    console.log('Code:', response.statusCode);
+    console.log('code:', response && response.statusCode);
   }
 });
