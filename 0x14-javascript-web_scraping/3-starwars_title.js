9a7d6a05a4d53d5d629655a45cac0bacdd49a802
Omar <omar0642624692@gmail.com>
2023-10-24 13:25:06 -0700

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 const movieID = process.argv[2];
-const api_url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
-let data;
+const apiurl = 'https://swapi-api.hbtn.io/api/films/' + movieID;
+let jsonData;
 
-request(api_url, function (err, response, body) {
+request(apiurl, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    json_data = JSON.parse(body);
-    console.log(json_data.title);
+    jsonData = JSON.parse(body);
+    console.log(jsonData.title);
   }
 });
